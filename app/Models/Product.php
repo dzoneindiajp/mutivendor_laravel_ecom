76f4bf799dd9f917c06b082d87f34975896a3a12
Jayant Sharma <jayant.sharma@techalmas.com>
2024-01-05 04:41:11 +0530

    Updated Code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -34,4 +34,5 @@ class Product extends Model
 
 
 
+
 }
--- config/settings.php
+++ config/settings.php
@@ -1,6 +1,7 @@
 <?php 
 config(["Reading.date_format"=>"Y-m-d"]);
 config(["Reading.records_per_page"=>"10"]);
+config(["Reading.default_currency"=>"$"]);
 config(["Site.copyright"=>"Copyright © 2023 Jaipurjewellaryhouse."]);
 config(["Site.fav_icon"=>"NOV2023/1699297022-settings.ico"]);
 config(["Site.from_email"=>"noreply@jaipurjewellary.com"]);
